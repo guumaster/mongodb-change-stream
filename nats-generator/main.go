07d34f71583ddfce512ec6d1f62fdf4adcd92ef5
Gustavo Marin <gustavo.marin@intelygenz.com>
2022-01-24 16:36:50 +0100

    change message publication loop

--- nats-generator/main.go
+++ nats-generator/main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
+	"os"
 	"time"
-  "os"
-  "fmt"
-  "math/rand"
 
-  "encoding/json"
+	"encoding/json"
 	stan "github.com/nats-io/stan.go"
 
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill-nats/pkg/nats"
 	"github.com/ThreeDotsLabs/watermill/message"
 
-  "github.com/brianvoe/gofakeit/v4"
+	"github.com/brianvoe/gofakeit/v4"
 )
 
 func main() {
-  gofakeit.Seed(time.Now().UnixNano())
+	gofakeit.Seed(time.Now().UnixNano())
 
-  natsURL := os.Getenv("STAN_URL")
+	natsURL := os.Getenv("STAN_URL")
 
-  if natsURL == "" {
-    natsURL = "nats://localhost:4222"
-  }
+	if natsURL == "" {
+		natsURL = "nats://localhost:4222"
+	}
 
 	publisher, err := nats.NewStreamingPublisher(
 		nats.StreamingPublisherConfig{
@@ -40,29 +40,28 @@ func main() {
 		panic(err)
 	}
 
-  // Publish a message per second
-  total := 0
-  for {
-    data := map[string]interface{}{
-      "msg": fmt.Sprintf("[%s] %s", os.Getenv("HOSTNAME"), gofakeit.HackerPhrase()),
-      "logLevel": gofakeit.LogLevel("general"),
-    }
-    str, err := json.Marshal(data)
-    if err != nil {
-      panic(err)
-    }
-    msg := message.NewMessage(watermill.NewUUID(), []byte(str))
-    
-    err = publisher.Publish("example.topic", msg)
-    if err != nil {
-      panic(err)
-    }
+	// Publish a message per second
+	total := 0
+	for {
+		data := map[string]interface{}{
+			"msg":      fmt.Sprintf("[%s] %s", os.Getenv("HOSTNAME"), gofakeit.HackerPhrase()),
+			"logLevel": gofakeit.LogLevel("general"),
+		}
+		str, err := json.Marshal(data)
+		if err != nil {
+			panic(err)
+		}
+		msg := message.NewMessage(watermill.NewUUID(), []byte(str))
 
-    total++
-    fmt.Printf("total %5d\n", total)
-    // random wait before next batch
-    timer := rand.Intn(500) + 300
-    time.Sleep(time.Duration(timer) * time.Millisecond)
-  }
+		err = publisher.Publish("GOLANG.topic", msg)
+		if err != nil {
+			panic(err)
+		}
+
+		total++
+		fmt.Printf("total %5d\n", total)
+		// random wait before next batch
+		timer := rand.Intn(500) + 2000
+		time.Sleep(time.Duration(timer) * time.Millisecond)
+	}
 }
-
